9eefd9c257bacefc6b82a3616d6cb451f655fbfa
AlexZab <alexey.zabolotniy.main@yandex.ru>
2022-12-07 14:13:23 +0100

    predictions parser

--- preds_parser.py
+++ preds_parser.py
@@ -86,5 +86,5 @@ if __name__ == '__main__':
 
     all_sumdf = pd.concat(all_sumdf).mean()
     all_confdf = pd.concat(all_confdf).mean()
-    sumdf.to_csv(os.path.join(iterator.history_path, f'summary.csv'))
-    confdf.to_csv(os.path.join(iterator.history_path, f'confusion.csv'))
+    sumdf.to_csv(os.path.join(iterator.history_path, f'{classification_name_formatted}_summary.csv'))
+    confdf.to_csv(os.path.join(iterator.history_path, f'{classification_name_formatted}_confusion.csv'))
